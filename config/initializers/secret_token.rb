cb54098b5e5881d9b5cc0e98ffe355aaf119fcb7
= <missoula@gmail.com>
2011-11-25 08:37:35 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneApp::Application.config.secret_token = '3d0ba78ab01a7e08268ecc253dc646f9ed985ba859f3eaab7ed8a14f172b95132bad89c79fcd8aae12a69f50b5cc68ac1fada2b7b13207204739fd1e450a4c7f'
